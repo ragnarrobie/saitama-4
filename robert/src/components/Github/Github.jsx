bb39751153e5899800694f2507a75d0419421cee
Robson Taye <ragnarrobie@gmail.com>
2025-03-05 08:59:37 +0300

    i am him

--- robert/src/components/Github/Github.jsx
+++ robert/src/components/Github/Github.jsx
@@ -3,8 +3,13 @@ import React from "react";
 function Github() {
   const [data, setdata] = React.useState(null);
   useEffect(() => {
-    fetch("");
-  });
+    fetch("https://github.com/ragnarrobie/saitama-4").then((response) =>
+      response.json.then((data) => {
+        console.log(data);
+        setData(data);
+      })
+    );
+  }, []);
   return (
     <div className="text-center bg-gray-700 text-white text-3xl p-4 ">
       Github
